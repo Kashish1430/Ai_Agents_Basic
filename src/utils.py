daa455fce8af21968b9f0c83ff9ad7a59e3cc505
Kashish1430 <kashishrajput95@yahoo.com>
2024-08-19 10:51:41 +0530

    Added prompt and tool

--- src/utils.py
+++ src/utils.py
@@ -9,3 +9,22 @@ def get_groq_api_key():
     
 def connect_to_groq():
     return Groq(api_key=get_groq_api_key())
+
+def get_prompt():
+    prompt = """
+    You run in a loop of THOUGHT, ACTION, PAUSE and OBSERVATION.
+    At the end of the loop you return an ANSWER.
+    Use Thought to showcase your thought process of the given query.
+    Use Action to chose one of the available and most suitable action and then return PAUSE.
+    OBSERVATION will be the result of you using those actions.
+
+    Your available actions are:
+    calculate:
+    e.g. calculate: 12^2 * 7^3
+    This action runs a calculation and returns a number as a result - This action uses python, so be sure to use floating point syntax wherever necessary
+
+
+    """
+
+def calculate(question):
+    return eval(question)
